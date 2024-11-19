2066a318c645fc762e65d1d31a82fc48fbe6adfe
aashraybavisa <aashray1042@gmail.com>
2024-11-19 09:46:41 -0500

    feat: web support added

--- components/jobdetails/about/About.jsx
+++ components/jobdetails/about/About.jsx
@@ -2,13 +2,13 @@ import { View, Text } from "react-native";
 
 import styles from "./about.style";
 
-const About = ({ info }) => {
+const About = ({ info, description }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.headText}>About the job:</Text>
 
       <View style={styles.contentBox}>
-        <Text style={styles.contextText}>{info}</Text>
+        <Text style={styles.contextText}>{description || info}</Text>
       </View>
     </View>
   );
