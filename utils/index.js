3d27f071d3ad37800557565dcb92a12c91d578b0
aashraybavisa <aashray1042@gmail.com>
2025-01-12 20:07:07 -0500

    feat: utils added

--- utils/index.js
+++ utils/index.js
@@ -1,8 +1,33 @@
 export const checkImageURL = (url) => {
-    if (!url) return false
-    else {
-        const pattern = new RegExp('^https?:\\/\\/.+\\.(png|jpg|jpeg|bmp|gif|webp)$', 'i');
-        return pattern.test(url);
-    }
+  if (!url) return false;
+  else {
+    const pattern = new RegExp(
+      "^https?:\\/\\/.+\\.(png|jpg|jpeg|bmp|gif|webp)$",
+      "i"
+    );
+    return pattern.test(url);
+  }
 };
 
+export const storeData = async (value) => {
+  try {
+    global.listing = value;
+    console.log("AsyncStorage-listing:storeData()->", value);
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem("listing", jsonValue);
+  } catch (e) {
+    // saving error
+  }
+};
+
+export const getData = async () => {
+  try {
+    const jsonValue = await AsyncStorage.getItem("listing");
+    const data = JSON.parse(jsonValue);
+    global.listing = data || null;
+    console.log("AsyncStorage-listing:getData()->", data);
+    return jsonValue != null ? data : null;
+  } catch (e) {
+    // error reading value
+  }
+};
